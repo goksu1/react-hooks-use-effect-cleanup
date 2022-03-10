c8ac41b1b67c90f17486dbd7398c5fa71ce9fcbd
goksu1 <goksu.baykal@gmail.com>
2022-03-10 10:52:49 +0300

    Done.

--- src/Clock.js
+++ src/Clock.js
@@ -3,8 +3,9 @@ import React, { useState, useEffect } from "react";
 function Clock() {
   const [time, setTime] = useState(new Date());
 
+  // the setInterval function we called in useEffect is still running in the background, and updating state every second.
   useEffect(() => {
-    setInterval(() => {
+    const timerID = setInterval(() => {
       setTime(new Date());
     }, 1000);
   }, []);
